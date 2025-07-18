6d92f437a199e03c8391e4c787a6fb2791062b64
Sparky579 <1779894826@qq.com>
2025-07-18 12:58:47 +0800

    change backend api

--- src/stores/blog.js
+++ src/stores/blog.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 
-const API_BASE_URL = 'https://38.12.36.246:24377/api'
+const API_BASE_URL = 'https://api.pekka.asia/api'
 // const API_BASE_URL = 'http://localhost:12377/api'
 
 export const useBlogStore = defineStore('blog', () => {
